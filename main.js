47862c1052ab74f9dbe6e09c8d344ffc5b7b5a3a
Okami2612 <51777169+Okami2612@users.noreply.github.com>
2021-10-14 12:11:07 +0700

    update navbar text

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 const images = document.querySelectorAll(".image");
 
-if(images) {
-  images.forEach(image => {
-    image.onclick =() => {
+if (images) {
+  images.forEach((image) => {
+    image.onclick = () => {
       document.querySelector(".active").classList.remove("active");
-      image.classList.add("active")
-    }
-  })
+      image.classList.add("active");
+    };
+  });
 }
+
+// Icon Navbar Glassmorphism Hover Effects
+let marker = document.querySelector("#marker");
+let list = document.querySelectorAll(".glassmorphism-hover-effects ul li");
+
+function moveIndicator(e) {
+  marker.style.left = e.offsetLeft + "px";
+  marker.style.width = e.offsetWidth + "px";
+}
+
+list.forEach((link) => {
+  link.addEventListener("mousemove", (e) => {
+    moveIndicator(e.target);
+  });
+});
+// Add active class in hover item list
+function activeLink() {
+  list.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
+}
+list.forEach((item) => item.addEventListener("mouseover", activeLink));
