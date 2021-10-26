ee1b89a9727d11e0a534ce752297b803258642fd
Okami2612 <51777169+Okami2612@users.noreply.github.com>
2021-10-26 17:51:18 +0700

    update

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Stretch Image
 const images = document.querySelectorAll(".image");
 
 if (images) {
@@ -29,3 +30,33 @@ function activeLink() {
   this.classList.add("active");
 }
 list.forEach((item) => item.addEventListener("mouseover", activeLink));
+
+// Navigation
+let list1 = document.querySelectorAll(".list");
+function setActiveClass() {
+  list1.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
+}
+list1.forEach((item)=>item.addEventListener('mouseover', setActiveClass))
+
+// Show Modals
+const showModal = (openButton, modalContent) =>{
+  const openBtn = document.getElementById(openButton),
+  modalContainer = document.getElementById(modalContent)
+
+  if(openBtn && modalContainer){
+    openBtn.addEventListener('click', ()=>{
+      modalContainer.classList.add('show-modal')
+    })
+  }
+}
+showModal('open-modal','modal-container')
+// Close modal
+const closeBtn = document.querySelectorAll('.close-modal');
+
+function closeModal(){
+  const modalContainer = document.getElementById('modal-container');
+  modalContainer.classList.remove('show-modal')
+}
+
+closeBtn.forEach(c => c.addEventListener('click', closeModal))
